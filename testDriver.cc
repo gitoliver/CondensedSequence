08a3f8f0a89d29f74458bcf835afccc3281f4fad
oliver <olivercgrant@gmail.com>
2021-03-25 17:52:37 +0100

    Updated testdriver to reflect new name of class and to test the reorderSequence functionality

--- testDriver.cc
+++ testDriver.cc
@@ -1,14 +1,15 @@
 
 #include <iostream>
-#include "condensedSequence.hpp"
+#include "sequenceManager.hpp"
 
 int main ()
 {	
-    std::string sequence = "DManp[2S,3Me]a1-6DManpa1-6[DGlcpNAcb1-4][DNeu5Aca2-6DGalpb1-4DGlcpNAc[3S]b1-2DManpa1-3]DManpb1-4DGlcpNAc[6Me]b1-4DGlcpNAcb1-OH";
+    std::string sequence = "DTalp[2S,3Me]a1-6DManpa1-6[DAllpNAcb1-4][DNeu5Aca2-6DGalpb1-4DGlcp[3S]b1-2DAltpa1-3]DManpb1-4DGulpNAc[6Me]b1-4DGlcpNAcb1-OH";
     std::cout << "ALKDJFLDAKJ\n";
-    CondensedSequence::CondensedSequence arryPotter(sequence);
+    CondensedSequence::SequenceManager arryPotter(sequence);
     std::cout << arryPotter.Print() << "\n";
     std::cout << "Visualize the graph here:\nhttps://dreampuf.github.io/GraphvizOnline/#digraph%20G%20%7B%0ABobie-%3ESteve%0A%7D\n";
+    arryPotter.reorderSequence();
     std::cout << "\n\n\nAbout to return.\n";
 	return 0;
 }
